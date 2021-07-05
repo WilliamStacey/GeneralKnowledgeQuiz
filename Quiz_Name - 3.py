5713b7888d353a89e1d112d9251da0723864019b
WilliamStacey <78886150+WilliamStacey@users.noreply.github.com>
2021-07-06 09:16:08 +1200

    Update Quiz_Name - 3.py

--- Quiz_Name - 3.py
+++ Quiz_Name - 3.py
@@ -1,8 +1,13 @@
 #ask name
+def name():
+    global name
+    name = ''
 
-while True:
-    name = input("Enter your name: ")
-    if name.isalpha():
-        break
-    print("Please enter characters A-Z only.")
+    while True:
+        name = input("Enter your name: ")
+        if name.isalpha():
+            break
+        print("Please enter characters A-Z only.")
 
+name()
+print("program complete")
