0c4cf339f3795c352cf155b2b424955f33c034b3
WilliamStacey <78886150+WilliamStacey@users.noreply.github.com>
2021-07-06 09:12:15 +1200

    Update Quiz_Age - 4.py

--- Quiz_Age - 4.py
+++ Quiz_Age - 4.py
@@ -1,10 +1,12 @@
 #ask age
-age = ''
+def age():
+    age = ''
 
-while True:
-    age = input("Enter your age: ")
-    if age.isnumeric():
-        break
-    print("Please enter whole numbers only.")
+    while True:
+          age = input("Enter your age: ")
+          if age.isnumeric():
+              break
+          print("Please enter numbers only.")
 
 print("You are",age)
+age()
