4a22fb0143b6e9bd3967c17a848b71fea87d74be
WilliamStacey <78886150+WilliamStacey@users.noreply.github.com>
2021-10-22 12:14:19 +1300

    Update Quiz_Age - 4.py

--- Quiz_Age - 4.py
+++ Quiz_Age - 4.py
@@ -3,10 +3,17 @@ def age():
     age = ''
 
     while True:
-          age = input("Enter your age: ")
-          if age.isnumeric():
-              break
-          print("Please enter numbers only.")
+      try:
+        age = int(input("Enter your age: "))
+        if age < 10:
+            print("You are too young to take this quiz.")
+            raise SystemExit
+        elif age > 18:
+            print("You are too old to take this quiz.")
+            raise SystemExit
+        break
+      except ValueError:
+        print("Please enter numbers only.")
 
 age()
 print("program complete")
