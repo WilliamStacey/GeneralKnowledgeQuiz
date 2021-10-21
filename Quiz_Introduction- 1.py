37beb170b8008c9a055c3deffb2d11015cffe341
WilliamStacey <78886150+WilliamStacey@users.noreply.github.com>
2021-10-22 11:39:05 +1300

    Update Quiz_Introduction- 1.py

--- Quiz_Introduction- 1.py
+++ Quiz_Introduction- 1.py
@@ -1,5 +1,6 @@
 #welcome to quiz
-print("*********Welcome to the General Knowledge Quiz*********")
-print("********This Quiz was made by me, William Stacey********")
+def credits():
+  print("*********Welcome to the General Knowledge Quiz*********")
+  print("********This Quiz was made by me, William Stacey********")
 
 print("program complete")
